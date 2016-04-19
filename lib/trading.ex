2d8c11e60c73b572636c890b256d73c8fe3fd71f
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-04-19 14:40:13 +0300

    added function for all trading api methods

--- lib/trading.ex
+++ lib/trading.ex
@@ -15,4 +15,78 @@ defmodule Poloniex.Trading do
   def return_ordertrades() do
   end
 
+
+  def buy() do
+
+  end
+
+  def sell() do
+
+  end
+
+  def cancelOrder() do
+
+  end
+
+  def moveOrder() do
+
+  end
+
+  def withdraw() do
+
+  end
+
+  def returnFeeInfo() do
+
+  end
+
+  def returnAvailableAccountBalances() do
+
+  end
+
+  def returnTradableBalances() do
+
+  end
+
+  def transferBalance() do
+
+  end
+
+  def returnMarginAccountSummary() do
+
+  end
+
+  def marginBuy() do
+
+  end
+
+  def marginSell() do
+
+  end
+
+  def getMarginPosition() do
+
+  end
+
+  def createLoanOffer() do
+
+  end
+
+  def cancelLoanOffer() do
+
+  end
+
+  def returnOpenLoanOffers() do
+
+  end
+
+  def returnActiveLoans() do
+
+  end
+
+  def toggleAutoRenew() do
+
+  end
+
+
 end
