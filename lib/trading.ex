b846e26fca8cc3ef933b1cc39d51194fa072aa04
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-04-19 13:49:42 +0300

    created poloniex trading module

---
+++ lib/trading.ex
@@ -0,0 +1,18 @@
+defmodule Poloniex.Trading do
+
+  def return_balances() do
+  end
+
+  def return_complete_balances() do
+  end
+
+  def return_openorders() do
+  end
+
+  def return_tradehistory() do
+  end
+
+  def return_ordertrades() do
+  end
+
+end
